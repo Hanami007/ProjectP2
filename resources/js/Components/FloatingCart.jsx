f2696cb147296724b0d170e9409c28a3f8223c21
Hanami007 <bass2545b@gmail.com>
2025-02-22 15:00:55 +0700

    อ่ะ

--- resources/js/Components/FloatingCart.jsx
+++ resources/js/Components/FloatingCart.jsx
@@ -4,7 +4,7 @@ import { Link } from "@inertiajs/react";  // ใช้ Link จาก Inertia.js
 const FloatingCart = () => {
   return (
     <Link
-      href="/cart"  // ตั้งค่าลิงก์ให้ไปที่หน้า Cart
+      href="/cart"
       className="fixed bottom-5 right-5 bg-blue-500 text-white p-3 rounded-full shadow-lg cursor-pointer hover:bg-blue-600 transition"
     >
       <ShoppingCart size={24} />
